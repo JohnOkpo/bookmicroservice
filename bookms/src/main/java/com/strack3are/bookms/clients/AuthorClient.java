08d7f757a92d3ceb32828b25736e80f8ae80e938
JohnOkpo <okpojohn11@gmail.com>
2024-01-03 01:42:17 +0100

    preparing to build docker image for all the services

--- bookms/src/main/java/com/strack3are/bookms/clients/AuthorClient.java
+++ bookms/src/main/java/com/strack3are/bookms/clients/AuthorClient.java
@@ -1,6 +1,5 @@
 package com.strack3are.bookms.clients;
 
-import com.strack3are.bookms.external.Author;
 import com.strack3are.bookms.external.AuthorRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name="AUTHOR-SERVICE")
+//@FeignClient(name="AUTHOR-SERVICE")
+@FeignClient( name="AUTHOR-SERVICE",  url="http://localhost:8082" )
 public interface AuthorClient {
 
     @GetMapping("api/v1/author/authorForBook/{id}")
